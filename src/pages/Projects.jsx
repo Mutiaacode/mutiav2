018f6f797737a33a965814e42c77eaf8e3b95282
Mutia Pegi Intanswari <149048644+Mutiaacode@users.noreply.github.com>
2023-10-30 05:04:30 +0700

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -50,14 +50,12 @@ export default function Card() {
             </button>
           </div>
           <div className="flex flex-col">
-{projects.map((project, index) => (
-  <a
+            {projects.map((project, index) => (
+  <div
     key={index}
     onMouseEnter={handleMouseEnter}
     onMouseLeave={handleMouseLeave}
     className="drop-shadow-md card bg-white rounded-lg px-5 py-3 gap-x-3 flex flex-col md:flex-none md:flex-row hover:-translate-y-1 hover:scale-100 duration-300 transition ease-in-out delay-150 hover:shadow-sm border border-gray-200 hover:border-gray-300"
-    href={project["github-url"]} 
-    target="_blank" 
   >
     <div className="rounded-full overflow-hidden flex items-center justify-center border border-gray-200 hidden md:block">
       <div className="card-image w-16 h-16 rounded-full overflow-hidden">
@@ -92,5 +90,5 @@ export default function Card() {
         />
       </svg>
     </button>
-  </a>
+  </div>
 ))}
